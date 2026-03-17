e0c68b122a4c4364840294201ca364c2bede6b6d
Bismit Wagle <bismitwagle@gmail.com>
2026-03-17 16:07:01 +0530

    feat: Generate access token per user

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,6 +1,6 @@
-import bcrypt from "bcryptjs"
+import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-import mongoose, { Schema, SchemaType } from "mongoose"
+import mongoose, { Schema } from "mongoose"
 
 
 const userSchema = new Schema(
@@ -89,4 +89,19 @@ userSchema.pre("save", async function (next) {
 //method to compare password
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
+}
+
+//generate access token (JWT issued per user)
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            name: this.name,
+        }, 
+        process.env.ACCESS_TOKEN_SECRET, 
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 }
