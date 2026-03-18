18755b955d670b5ec123b8e01ce7bc4369021c3a
Bismit Wagle <bismitwagle@gmail.com>
2026-03-18 17:44:48 +0530

    feat: link to the user who booked the appointment

--- backend/src/models/appointment.models.js
+++ backend/src/models/appointment.models.js
@@ -3,6 +3,12 @@ import mongoose, { Schema } from "mongoose";
 
 const appointmentSchema = new Schema(
     {
+        //Link to the user who booked
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
         
     }
 )
