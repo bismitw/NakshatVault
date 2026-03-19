7db4e18fea20f6fd3454ed552cd89a61448d9675
Bismit Wagle <bismitwagle@gmail.com>
2026-03-19 14:12:53 +0530

    feat: pass data in constructor parameter

--- backend/src/utils/apiError.utils.js
+++ backend/src/utils/apiError.utils.js
@@ -3,6 +3,7 @@ class ApiError extends Error {
         statusCode,
         message= "Something went wrong",
         errors= [],
+        data= null,
         stack= ""
     ){
         super(message)
@@ -15,7 +16,7 @@ class ApiError extends Error {
         if(stack) {
             this.stack = stack
         } else {
-            Error.captureStackTrace(this, this.construtor)
+            Error.captureStackTrace(this, this.constructor)
         }
     }
 }
