637720c2164680759d9edb857905213d7e17f6f7
Bismit Wagle <bismitwagle@gmail.com>
2026-03-23 16:42:17 +0530

    feat: Register user controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,10 +1,21 @@
 import { registerUserService } from "../services/auth.services";
 import { ApiResponse } from "../utils/apiResponse.utils.js";
-import { AsyncHandler } from "../utils/asyncHandler.utils.js";
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
 
 
 const registerUser  = asyncHandler( async(req, res)=> {
     const {fullName, email, password, phone} = req.body;
     
+    const user = await registerUserService({
+        fullName, 
+        email,
+        password,
+        phone,
+    });
 
+    return res
+    .status(201)
+    .json(new ApiResponse(201,user, "User registered Successfully"))
 })
+
+export {registerUser}
