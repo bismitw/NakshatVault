cba850574e07227bc691aa023ef283cfff2aa5b3
Bismit Wagle <bismitwagle@gmail.com>
2026-03-23 16:59:57 +0530

    feat: mount route in app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,9 @@
     import cookieParser from "cookie-parser";
     import helmet from "helmet"
     import compression from "compression";
+    import authRouter from "./src/routes/auth.routes.js"
+
+
     const app = express()
 
     app.use(
@@ -13,10 +16,16 @@
         })
     )
 
+    //common middleware
     app.use(helmet())
     app.use(compression())
 
-
     app.use(express.json({limit:"16kb"}))
     app.use(cookieParser());
+
+
+    //routes
+    app.use("/api/v1/auth", authRouter)
+
+
     export {app}
