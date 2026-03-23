cf115e22a31be172e7a25e409c3b6d457a6faf50
Bismit Wagle <bismitwagle@gmail.com>
2026-03-23 16:11:59 +0530

    fix: fix some of the issue with auth service

--- backend/src/services/auth.services.js
+++ backend/src/services/auth.services.js
@@ -6,24 +6,26 @@ const registerUserService = async({fullName, email, password, phone}) => {
         throw new ApiError(400, "FullName, email and password are required");
     }
 
-    const existingUser = await User.findOne({email: email.tolowercase().trim()});
+    const existingUser = await User.findOne({email: email.toLowerCase().trim()});
 
     if(existingUser){
         throw new ApiError(400, "User with this email already exists");
     }
 
-    const user = User.create({
+    const user = await User.create({
         fullName: fullName.trim(),
         email: email.tolowercase().trim(),
         password,
-        phone: phone.trim()|| "",
+        phone: phone?.trim()|| "",
     })
 
-    const createdUser = await User.findById(user._id).select("-password,-refreshToken");
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
 
     if(!createdUser){
-        throw new error(500,"Failed to create User")
+        throw new ApiError(500,"Failed to create User")
     }
 
     return createdUser;
 }
+
+export {registerUserService}
