8ff5763466eff17456f9cc676b16ce146fc27b4d
Bismit Wagle <bismitwagle@gmail.com>
2026-03-23 13:48:11 +0530

    feat: Initiate register User service

--- backend/src/services/auth.services.js
+++ backend/src/services/auth.services.js
@@ -1,4 +1,9 @@
 import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/apiError.utils.js"
 
-const registerUserService = async({name, email, password, })
+const registerUserService = async({fullName, email, password, phone}) => {
+    if(!fullName || !email || !password){
+        throw new ApiError(400, "FullName, email and password are required");
+    }
+
+}
