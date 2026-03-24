21530cdd8572696eb1d50c0eae47f9579d9cdabb
Bismit Wagle <bismitwagle@gmail.com>
2026-03-24 17:16:57 +0530

    feat: read access token from cookie

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -4,3 +4,8 @@ import { ApiError } from "../utils/apiError.utils";
 import { asyncHandler } from "../utils/asyncHandler.utils";
 
 
+const verifyJWT = asyncHandler(async(req, res, next) => {
+    const token = req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer", "");
+
+})
