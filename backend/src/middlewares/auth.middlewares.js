10e3cff41b81672995021eeacf88dde37d8d9af3
Bismit Wagle <bismitwagle@gmail.com>
2026-03-24 16:51:11 +0530

    feat: import for protected authentication

---
+++ backend/src/middlewares/auth.middlewares.js
@@ -0,0 +1,6 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models";
+import { ApiError } from "../utils/apiError.utils";
+import { asyncHandler } from "../utils/asyncHandler.utils";
+
+
