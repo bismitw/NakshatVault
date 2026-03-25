43d588cacc2a5dedb89235c29b3a44153b2db1e2
Bismit Wagle <bismitwagle@gmail.com>
2026-03-25 12:08:06 +0530

    feat: check conditional for user and provide req user as user and export

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.models";
-import { ApiError } from "../utils/apiError.utils";
-import { asyncHandler } from "../utils/asyncHandler.utils";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/apiError.utils.js";
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
 
 
 const verifyJWT = asyncHandler(async(req, res, next) => {
     const token = req.cookies?.accessToken ||
-    req.header("Authorization")?.replace("Bearer", "");
+    req.header("Authorization")?.replace("Bearer ", "");
 
     if(!token){
         throw new ApiError(401, "Unauthorized request");
@@ -14,4 +14,13 @@ const verifyJWT = asyncHandler(async(req, res, next) => {
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+    if(!user){
+        throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user ;
+    next();
 })
+
+export {verifyJWT};
