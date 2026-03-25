de0b070a78056dff20b90503007fe920b3328780
Bismit Wagle <bismitwagle@gmail.com>
2026-03-25 11:03:45 +0530

    feat: check user with decoded token and deselect password and refreshToken

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -13,4 +13,5 @@ const verifyJWT = asyncHandler(async(req, res, next) => {
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
 })
