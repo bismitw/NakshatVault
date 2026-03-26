ba06130ff126fe0197447743d6870cfe0fda8e46
Bismit Wagle <bismitwagle@gmail.com>
2026-03-26 14:22:15 +0530

    feat: create refresh token

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import {Router} from "express";
-import { loginUser, registerUser, getCurrentUser, logoutUser } from "../controllers/auth.controllers.js";
+import { loginUser, registerUser, getCurrentUser, logoutUser, refreshAccessToken } from "../controllers/auth.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { logoutUserService } from "../services/auth.services.js";
 
 const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/me").get(verifyJWT, getCurrentUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
