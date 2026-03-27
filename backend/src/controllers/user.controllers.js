e7d528d733bfa0bec0c92cfb01ddca318ab264dd
Bismit Wagle <bismitwagle@gmail.com>
2026-03-27 11:59:20 +0530

    feat: User controller to fetch profile

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,1 +1,10 @@
-import {ApiResponse} from "../utils/apiResponse.utils.js"
+import { ApiResponse } from "../utils/apiResponse.utils.js"
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    return res 
+    .status(200)
+    .json(new ApiResponse(200, req.user, "User profile fetched successfully"));
+});
+
+export { getUserProfile };
