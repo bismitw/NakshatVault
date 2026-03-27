fbaef202b71f0a1c942cdd9690561e625f9b2754
Bismit Wagle <bismitwagle@gmail.com>
2026-03-27 16:02:06 +0530

    feat: update user profile in user controller and also export it

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import { ApiResponse } from "../utils/apiResponse.utils.js"
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import { updateUserProfileService } from "../services/user.services.js";
 
 const getUserProfile = asyncHandler(async (req, res) => {
     return res 
@@ -7,4 +8,12 @@ const getUserProfile = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, req.user, "User profile fetched successfully"));
 });
 
-export { getUserProfile };
+const updateUserProfile = asyncHandler(async(req, res) => {
+    const updatedUser = await updateUserProfileService(req.user?._id, req.body);
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedUser, "User Profile updated successfully"))
+})
+
+export { getUserProfile, updateUserProfile };
