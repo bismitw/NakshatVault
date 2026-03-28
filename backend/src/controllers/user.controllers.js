69c0dede3e40fb2e0cfb4f10f0ab44b60fd3ab1e
Bismit Wagle <bismitwagle@gmail.com>
2026-03-28 14:13:04 +0530

    feat: update birth details

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,6 +1,6 @@
 import { ApiResponse } from "../utils/apiResponse.utils.js"
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
-import { updateUserProfileService } from "../services/user.services.js";
+import { updateUserProfileService, updateBirthDetailsService } from "../services/user.services.js";
 
 const getUserProfile = asyncHandler(async (req, res) => {
     return res 
@@ -16,4 +16,11 @@ const updateUserProfile = asyncHandler(async(req, res) => {
     .json(new ApiResponse(200, updatedUser, "User Profile updated successfully"))
 })
 
-export { getUserProfile, updateUserProfile };
+const updateBirthDetails = asyncHandler(async(req, res) => {
+    const updatedUser = await updateBirthDetailsService(req.user?._id, req.body);
+    return res 
+    .status(200)
+    .json(new ApiResponse(200, updatedUser, "Birth details updated successfully"))
+})
+
+export { getUserProfile, updateUserProfile, updateBirthDetails };
