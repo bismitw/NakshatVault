6d2bf3943f46de149bb7e6f1bbda399fdd5ed493
Bismit Wagle <bismitwagle@gmail.com>
2026-03-30 14:35:23 +0530

    feat: createKundli controller and export

--- backend/src/controllers/kundli.controllers.js
+++ backend/src/controllers/kundli.controllers.js
@@ -2,4 +2,12 @@ import { createKundliService } from "../services/kundli.services.js";
 import { ApiResponse } from "../utils/apiResponse.utils.js";
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
 
+const createKundli = asyncHandler(async (req, res) => {
+    const kundli = await createKundliService(req.user?._id, req.body);
 
+    return res
+    .status(201)
+    .json(new ApiResponse(201, kundli, "Kundli created successfully"));
+});
+
+export { createKundli };
