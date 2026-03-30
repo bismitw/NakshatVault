155be8fc9ac87c5ac35fc558d447f28a3b25dd2f
Bismit Wagle <bismitwagle@gmail.com>
2026-03-30 11:28:11 +0530

    feat: intialize kundli service with conditional and destructure kundliData

--- backend/src/services/kundli.services.js
+++ backend/src/services/kundli.services.js
@@ -2,3 +2,10 @@ import {Kundli} from "../models/kundli.models.js"
 import {User} from "../models/user.models.js"
 import { ApiError } from "../utils/apiError.utils.js"
 
+const createKundliService = async ( userId, kundliData ) => {
+    if(!userId){
+        throw new ApiError(400, "User id is required");
+    }
+
+    const{ title, description, dateOfBirth, timeOfBirth, placeOfBirth} = kundliData;
+}
