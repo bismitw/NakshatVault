ca9ef188652eb4627af68428ab38c49846bc58d0
Bismit Wagle <bismitwagle@gmail.com>
2026-04-01 16:58:11 +0530

    feat: get user Appointments route

--- backend/src/routes/appointment.routes.js
+++ backend/src/routes/appointment.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createAppointment } from "../controllers/appointment.controllers.js";
+import { createAppointment, getUserAppointments } from "../controllers/appointment.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
-router.route("/").post(verifyJWT, createAppointment);
+router
+    .route("/")
+    .post(verifyJWT, createAppointment)
+    .get(verifyJWT, getUserAppointments);
 
 export default router;
