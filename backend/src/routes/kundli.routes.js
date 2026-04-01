ad46f9b8a4ee4b6c260443ed69357b3657f0cb35
Bismit Wagle <bismitwagle@gmail.com>
2026-04-01 11:50:21 +0530

    feat: delete route"

--- backend/src/routes/kundli.routes.js
+++ backend/src/routes/kundli.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createKundli, getKundliById, getUserKundlis, updateKundli } from "../controllers/kundli.controllers.js";
+import { createKundli, deleteKundli, getKundliById, getUserKundlis, updateKundli } from "../controllers/kundli.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
@@ -11,6 +11,6 @@ router.route("/")
 router.route("/:id")
 .get(verifyJWT, getKundliById)
 .patch(verifyJWT, updateKundli)
-
+.delete(verifyJWT, deleteKundli)
 
 export default router;
