3a0eada6b51f77bb61f1dbab8b2d732b8181111b
Bismit Wagle <bismitwagle@gmail.com>
2026-04-01 12:12:52 +0530

    feat: initialize appointment service and conditional check for user id

--- backend/src/services/appointment.services.js
+++ backend/src/services/appointment.services.js
@@ -1,3 +1,9 @@
 import {Appointment} from "../models/appointment.models.js"
 import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/apiError.utils.js"
+
+const createAppointmentService = async (userId, appointmentData) => {
+    if(!userId){
+        throw new ApiError(400, "UserId is required");
+    }
+}
