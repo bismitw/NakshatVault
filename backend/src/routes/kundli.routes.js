ecfe917002c8cb2edca65738c1cba0d5ff7bc2cd
Bismit Wagle <bismitwagle@gmail.com>
2026-04-02 17:21:14 +0530

    feat: validate required fields

--- backend/src/routes/kundli.routes.js
+++ backend/src/routes/kundli.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createKundli, deleteKundli, getKundliById, getUserKundlis, updateKundli } from "../controllers/kundli.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { validateRequiredFields } from "../middlewares/validate.middlewares.js";
 
 const router = Router();
 
 router.route("/")
-.post(verifyJWT, createKundli)
+.post(verifyJWT, validateRequiredFields(["dateOfBirth", "timeOfBirth", "placeOfBirth"]),createKundli)
 .get(verifyJWT, getUserKundlis)
 
 router.route("/:id")
