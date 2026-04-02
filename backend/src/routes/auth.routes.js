60f8b0bf3d974c1b6a84cdcb3d898133e30134b4
Bismit Wagle <bismitwagle@gmail.com>
2026-04-02 16:31:44 +0530

    feat: impor validate required fields and refreshToken

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,14 @@
 import {Router} from "express";
 import { loginUser, registerUser, getCurrentUser, logoutUser, refreshAccessToken } from "../controllers/auth.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { validateRequiredFields } from "../middlewares/validate.middlewares.js";
 
 const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/me").get(verifyJWT, getCurrentUser);
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
+router.route("/refresh-token")
+.post(validateRequiredFields(["refreshToken"]), refreshAccessToken);
 
 export default router;
