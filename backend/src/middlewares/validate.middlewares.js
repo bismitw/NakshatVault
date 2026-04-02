f94764ebcedbf2a3d208f52a047e3e97c82d670a
Bismit Wagle <bismitwagle@gmail.com>
2026-04-02 15:53:58 +0530

    feat: missing fields with return

--- backend/src/middlewares/validate.middlewares.js
+++ backend/src/middlewares/validate.middlewares.js
@@ -2,6 +2,10 @@ import { ApiError } from "../utils/apiError.utils";
 
 const validateRequiredFields = (fields) => {
     return (req, res, next) => {
-        
+        const missingFields = fields.filter((field) => {
+            const value = req.body?.[field];
+
+            return value === undefined || value === null || value === "";
+        })
     }
 }
