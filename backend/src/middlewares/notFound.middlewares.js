5267324f1656e0efd9b2ac25a3e613872a9df1d7
Bismit Wagle <bismitwagle@gmail.com>
2026-04-03 20:47:30 +0530

    feat: add api error for no route found

--- backend/src/middlewares/notFound.middlewares.js
+++ backend/src/middlewares/notFound.middlewares.js
@@ -1,5 +1,7 @@
 import { ApiError } from "../utils/apiError.utils.js";
 
 const notFoundHandler = (req, res, next) => {
-    
+    next(new ApiError(404, `Route not found: ${req.originalUrl}`));
 }
+
+export {notFoundHandler}
