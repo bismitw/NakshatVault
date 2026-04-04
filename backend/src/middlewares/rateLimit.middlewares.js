8aa8483649ed1df990d12b39322971ac12f37744
Bismit Wagle <bismitwagle@gmail.com>
2026-04-04 12:45:36 +0530

    feat: protection for many request at a time

--- backend/src/middlewares/rateLimit.middlewares.js
+++ backend/src/middlewares/rateLimit.middlewares.js
@@ -1,5 +1,16 @@
 import ratelimit from "express-rate-limit"
 
 const authRateLimiter = ratelimit({
-    
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: {
+    success: false,
+    message: "Too many auth requests, please try again later",
+    errors: [],
+    data: null,
+    },
 })
+
+export {authRateLimiter}
