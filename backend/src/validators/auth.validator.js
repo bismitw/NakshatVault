5c73815022337b2d742cde0743572a9992c1071c
Bismit Wagle <bismitwagle@gmail.com>
2026-04-06 10:43:48 +0530

    feat: check for required fullname email and password

--- backend/src/validators/auth.validator.js
+++ backend/src/validators/auth.validator.js
@@ -3,5 +3,8 @@ import { ApiError } from "../utils/apiError.utils.js";
 const validateRegisterInput =  (req, res, next) => {
     const {fullName, email, password} = req.body;
 
-    
+    if(!fullName || !email || !password){
+        return next(new ApiError(400, "FullName, Email and Password are required"));
+    }
+
 }
