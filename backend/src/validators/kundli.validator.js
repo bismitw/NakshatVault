68711915ea9119c906b08a568e4786ffc0082385
Bismit Wagle <bismitwagle@gmail.com>
2026-04-07 17:21:48 +0530

    feat: check if anything missing and provide error

--- backend/src/validators/kundli.validator.js
+++ backend/src/validators/kundli.validator.js
@@ -3,3 +3,10 @@ import { ApiError } from "../utils/apiError.utils";
 const validateKundliInput = (req,res,next) => {
     const {title, description, dateOfBirth, timeOfBirth, placeOfBirth} = req.body
 }
+
+if(!dateOfBirth || !timeOfBirth || !placeOfBirth){
+    return next (
+        new ApiError(400,"Date of birth, time of birth, and place of birth are required"
+    )
+    )
+}
