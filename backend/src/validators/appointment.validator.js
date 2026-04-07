9a8b17356dbbb180f32f28f2b893d244a4bfa9d4
Bismit Wagle <bismitwagle@gmail.com>
2026-04-07 15:09:44 +0530

    feat: initialize validateAppointmentInput, destructure req.body and check if anything is missing

--- backend/src/validators/appointment.validator.js
+++ backend/src/validators/appointment.validator.js
@@ -11,3 +11,14 @@ const allowedConsultationTypes = [
 
 const allowedModes = ["Online", "Offline"];
 const allowedStatuses = ["Booked", "In Review", "Approved", "Cancelled"];
+
+const validateAppointmentInput = (req, res, next) => {
+
+    const {expertName, date, timeSlot, consultationType, mode} = req.body;
+
+    if(!expertName || !date ||timeSlot){
+        return next(
+            new ApiError(400, "Expert name, date, and time slot are required")
+        );
+    }
+}
