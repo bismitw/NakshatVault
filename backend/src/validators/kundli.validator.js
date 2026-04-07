df1d4dbf556f901c1222cc3bc1fc7b14154a3383
Bismit Wagle <bismitwagle@gmail.com>
2026-04-07 17:10:17 +0530

    feat: initialize validatekundliInput and destructure req.boy

--- backend/src/validators/kundli.validator.js
+++ backend/src/validators/kundli.validator.js
@@ -1,2 +1,5 @@
 import { ApiError } from "../utils/apiError.utils";
 
+const validateKundliInput = (req,res,next) => {
+    const {title, description, dateOfBirth, timeOfBirth, placeOfBirth} = req.body
+}
