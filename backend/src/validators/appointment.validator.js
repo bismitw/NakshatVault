dbc485bbec959438b4ccb351258a56c6ad4c8ed6
Bismit Wagle <bismitwagle@gmail.com>
2026-04-07 14:43:00 +0530

    feat: import apierror, initialize allowed consultation types for appointment

---
+++ backend/src/validators/appointment.validator.js
@@ -0,0 +1,11 @@
+import { ApiError } from "../utils/apiError.utils.js";
+
+const allowedConsultationTypes = [
+    "General",
+    "Marriage",
+    "Career",
+    "Finance",
+    "Health",
+    "Other",
+];
+
