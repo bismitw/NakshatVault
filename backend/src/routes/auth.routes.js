de3f69735b696c92185f9334608bdc65015b9d99
Bismit Wagle <bismitwagle@gmail.com>
2026-04-09 12:28:07 +0530

    refactor: remove validate required fields

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,6 @@
 import {Router} from "express";
 import { loginUser, registerUser, getCurrentUser, logoutUser, refreshAccessToken } from "../controllers/auth.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { validateRequiredFields } from "../middlewares/validate.middlewares.js";
 import { authRateLimiter } from "../middlewares/rateLimit.middlewares.js";
 import { validateRegisterInput, validateLoginInput } from "../validators/auth.validator.js";
 
@@ -16,6 +15,6 @@ router.route("/me").get(verifyJWT, getCurrentUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token")
-.post(authRateLimiter, validateRequiredFields(["refreshToken"]), refreshAccessToken);
+.post(authRateLimiter, refreshAccessToken);
 
 export default router;
