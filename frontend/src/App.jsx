3606977cf09f3a6d97a5c235504463f468544e34
Bismit Wagle <bismitwagle@gmail.com>
2026-04-10 12:41:07 +0530

    feat: import route, routes and Homepage along with initializing main route for Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/Homepage.jsx'
 
 function App() {
 
   return (
-    <>
-
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App
