a23295cff199133d393a507f5e1bc61e1b152063
Bismit Wagle <bismitwagle@gmail.com>
2026-04-11 19:39:06 +0530

    feat: api request handler with returning data

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,23 @@
 const API_BASE_URL =
     import.meta.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
 
+async function  apiRequest(endpoint, options={}) {
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            ...(options.headers || {}),
+        },
+        ...options,
+    })
+
+    const data = await response.json();
+
+    if(!response.ok){
+        throw new Error(data.message || "Something went wrong");
+    }
+
+    return data;
+}
+
+export {apiRequest, API_BASE_URL};
