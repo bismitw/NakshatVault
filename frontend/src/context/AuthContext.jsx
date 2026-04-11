55818c85f89f81c46c2e764b736554b18e7c79ca
Bismit Wagle <bismitwagle@gmail.com>
2026-04-11 20:32:26 +0530

    feat: initialize auth context and auth provider function

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,2 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { apiRequest } from "../services/api.js";
+
+const AuthContext = createContext(null);
+
+function AuthProvider  ({ children }) {}
+
