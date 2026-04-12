4d2c8542c23f97cc29efd39fb5e063512b2e80af
Bismit Wagle <bismitwagle@gmail.com>
2026-04-12 22:49:35 +0530

    feat: initialize loginPage function, use useState hook for setForm data and submitting

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,3 +2,14 @@ import { useState } from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {useAuth} from "../context/AuthContext.jsx";
 
+function LoginPage(){
+    const navigate = useNavigate();
+    const {Login} = useAuth();
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [submitting, setSubmitting] = useState(false);
+}
