012deb5e400f1bd27d02434ac708c7f630269cde
Bismit Wagle <bismitwagle@gmail.com>
2026-04-12 15:52:38 +0530

    feat: read login infor from auth context and add loading message

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -2,5 +2,14 @@ import {Navigate} from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
 
 function ProtectedRoute({ children }) {
-    
+
+    const {user, authloading} = useAuth();
+
+    if(authloading){
+        return (
+            <div className="flex min-h-screen items-center justify-center text-stone-200">
+                Checking Session
+            </div>
+        )
+    }
 }
