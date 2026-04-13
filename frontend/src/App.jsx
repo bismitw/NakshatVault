e6ebcfc71bd5a96311fd201f647cb5d5798d6a72
Bismit Wagle <bismitwagle@gmail.com>
2026-04-13 19:03:45 +0530

    feat: import page routes and integrate it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import DashBoardPage from './pages/DashBoardPage.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route 
+      path="/dashboard" 
+      element={
+        <ProtectedRoute>
+          <DashBoardPage />
+        </ProtectedRoute>
+      } />
     </Routes>
   );
 }
