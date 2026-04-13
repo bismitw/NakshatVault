9193efd65a22e7b2ff2b4274a13b729d48df4132
Bismit Wagle <bismitwagle@gmail.com>
2026-04-13 19:06:14 +0530

    refactor: add .env in api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-    import.meta.VITE_API_BASE_URL || "http://localhost:8001/api/v1";
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:8001/api/v1";
 
 async function  apiRequest(endpoint, options={}) {
     const response = await fetch(`${API_BASE_URL}${endpoint}`, {
