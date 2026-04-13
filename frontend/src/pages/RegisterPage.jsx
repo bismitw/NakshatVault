e92a9b8d26cff9ce08b683f369ebe60c5d1c9854
Bismit Wagle <bismitwagle@gmail.com>
2026-04-13 14:28:24 +0530

    feat: initialize registerPage function

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,3 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
+
+function RegisterPage() { 
+    
+}
