7130a72a46ae025a6b2af24395290513f673d2a7
Bismit Wagle <bismitwagle@gmail.com>
2026-04-13 18:36:33 +0530

    feat: initialize handlelogout with async

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -3,5 +3,7 @@ import { useAuth } from "../context/AuthContext.jsx";
 function DashBoardPage() { 
     const {user, logout} = useAuth();
 
-
+    const handleLogout = async () => {
+        await logout();
+    }
 }
