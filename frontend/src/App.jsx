723c2cb4efef45345d821ed23ae26dd127f02cb8
Bismit Wagle <bismitwagle@gmail.com>
2026-04-14 14:24:43 +0530

    typo: DashBoard to Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
-import DashBoardPage from './pages/DashBoardPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
 import RegisterPage from './pages/RegisterPage.jsx'
 import ProtectedRoute from './routes/ProtectedRoute.jsx'
 
@@ -17,7 +17,7 @@ function App() {
       path="/dashboard" 
       element={
         <ProtectedRoute>
-          <DashBoardPage />
+          <DashboardPage />
         </ProtectedRoute>
       } />
     </Routes>
