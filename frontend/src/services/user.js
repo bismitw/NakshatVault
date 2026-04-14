bbe219a5ebc31493ccd24a48b4acaf351440cc31
Bismit Wagle <bismitwagle@gmail.com>
2026-04-14 14:31:05 +0530

    refactor: add payload as a parameter and fix typo: user to users

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -1,19 +1,19 @@
 import { apiRequest } from "./api.js";
 
 async function getUserProfile() {
-    return apiRequest("/user/profile", {
+    return apiRequest("/users/profile", {
         method: "GET",
     });
 }
 
-async function updateUserProfile(){
+async function updateUserProfile(payload){
     return apiRequest("/users/profile", {
         method: "PATCH",
         body: JSON.stringify(payload),
     });
 }
 
-async function updateBirthDetails() {
+async function updateBirthDetails(payload) {
     return apiRequest("/users/birth-details", {
         method: "PATCH",
         body: JSON.stringify(payload),
