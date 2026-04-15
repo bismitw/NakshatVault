ff1d3f554d66913c3d7a8834d57b1faf0cc6388e
Bismit Wagle <bismitwagle@gmail.com>
2026-04-15 11:44:02 +0530

    feat: handle logout with success and error

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -5,4 +5,18 @@ import { useAuth } from "../context/AuthContext.jsx";
 function AdminDashboardPage() {
     const {user, authLoading} = useAuth();
     const navigate = useNavigate();
+
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            toast.success("Logged out successfully");
+            navigate("/");
+        } catch (error) {
+            toast.error(error.message || "Logout failed");
+        }
+    }
+
+
+    
 }
