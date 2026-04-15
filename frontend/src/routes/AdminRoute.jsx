69eb8276fb5aa72a9d0372c6ada9fe997608cadf
Bismit Wagle <bismitwagle@gmail.com>
2026-04-15 11:29:05 +0530

    feat: initialize adminRoute function, destructure user and authLoading from useAuth, check authloading

--- frontend/src/routes/AdminRoute.jsx
+++ frontend/src/routes/AdminRoute.jsx
@@ -1,2 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+
+
+function AdminRoute({children}) {
+    const {user, authLoading} = useAuth();
+
+    if (authLoading){
+        return (
+            <div className="flex min-h-screen items-center justify-center text-stone-200">
+            Checking admin session...
+            </div>
+        );
+    }
+}
