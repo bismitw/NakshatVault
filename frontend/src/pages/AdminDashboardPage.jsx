3e99d52cb2347fb71fbed8ee9a5a480fa94c09f4
Bismit Wagle <bismitwagle@gmail.com>
2026-04-15 11:40:02 +0530

    feat: initialize AdminDashboardPage function, destructure user and authLoading from useAuth, declare navigate for useNavigate

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -1,4 +1,8 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useAuth } from "../context/AuthContext.jsx";
 
+function AdminDashboardPage() {
+    const {user, authLoading} = useAuth();
+    const navigate = useNavigate();
+}
