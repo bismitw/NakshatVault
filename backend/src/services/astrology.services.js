26c730ce3f5376b603f7627073bba051b6054333
Bismit Wagle <bismitwagle@gmail.com>
2026-04-16 13:30:51 +0530

    feat: initialize getProkeralaAccessToken, post request

--- backend/src/services/astrology.services.js
+++ backend/src/services/astrology.services.js
@@ -1,3 +1,27 @@
 import axios from "axios" 
 import { astrologyApi } from "../config/astrology.js"
 import { ApiError } from "../utils/apiError.utils.js"
+
+
+const getProkeralaAccessToken = async () => {
+    try {
+        const tokenResponse = await axios.post(
+            "https://api.prokerala.com/token",
+
+            new URLSearchParams({
+                grant_type: "client_credentials",
+                client_id: process.env.PROKERALA_CLIENT_ID,
+                client_secret: process.env.PROKERALA_CLIENT_SECRET,
+            }).toString(),
+            {
+                headers: {
+                    "Content_type":"application/x-www-form-urlencoded",
+                },
+            }
+        );
+
+        return tokenResponse.data?.access_token;
+    } catch (error) {
+        
+    }
+}
