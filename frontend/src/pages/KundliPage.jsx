853b360f7b62f95cdc0d7844e28e4637b993de0b
Bismit Wagle <bismitwagle@gmail.com>
2026-04-19 20:35:50 +0530

    feat: create state variables for kundli loading and submitting

--- frontend/src/pages/KundliPage.jsx
+++ frontend/src/pages/KundliPage.jsx
@@ -2,3 +2,9 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 import { generateKundli, getKundlis } from "../services/kundli.js";
+
+function kundliPage() {
+    const [kundlis, setKundlis] = useState([]);  
+    const [loading, setLoading] = useState(true);
+    const [submitting, setSubmitting] = useState(false);
+}
