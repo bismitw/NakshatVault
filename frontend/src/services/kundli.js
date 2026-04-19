b6240f81ce3135cfad781befcaf523bf1b602c2c
Bismit Wagle <bismitwagle@gmail.com>
2026-04-19 20:09:55 +0530

    feat: function for fetch and generate kundli, export both functions

--- frontend/src/services/kundli.js
+++ frontend/src/services/kundli.js
@@ -1,1 +1,16 @@
 import { apiRequest } from "./api.js"
+
+async function getKundlis() {
+    return apiRequest("/kundli", {
+        method: "GET",
+    })
+}
+
+async function generateKundli(payload) {
+    return apiRequest("/kundli/generate", {
+        method: "POST",
+        body: JSON.stringify(payload),
+    });
+}
+
+export {getKundlis, generateKundli};
