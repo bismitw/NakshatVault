5b30482f0f1e32b7a8a4200810ca2c5ee0690751
Bismit Wagle <bismitwagle@gmail.com>
2026-04-19 20:24:53 +0530

    feat: import hooks, link, toast and generatekundlis and getkundlis

---
+++ frontend/src/pages/KundliPage.jsx
@@ -0,0 +1,4 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { generateKundli, getKundlis } from "../services/kundli.js";
