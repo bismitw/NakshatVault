3df1c5a6b7366d5d6e3dabdf685fb7489cbbf4a2
Bismit Wagle <bismitwagle@gmail.com>
2026-04-20 12:22:10 +0530

    feat: import  react hooks router toast and kundli api requests

---
+++ frontend/src/pages/KundliDetailPage.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import {
+    deleteKundli,
+    getKundliById,
+    updateKundli,
+} from "../services/kundli.js";
