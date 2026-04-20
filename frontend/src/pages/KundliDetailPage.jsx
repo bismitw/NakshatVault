3c375517aab2796007f2603dca18402789f0b4fa
Bismit Wagle <bismitwagle@gmail.com>
2026-04-20 12:31:00 +0530

    feat: create state variable with their default values

--- frontend/src/pages/KundliDetailPage.jsx
+++ frontend/src/pages/KundliDetailPage.jsx
@@ -11,5 +11,16 @@ function KundliDetailPage() {
     const {id} = useParams();
     const navigate = useNavigate();
 
-    
+    const [kundli, setKundli] = useState(null);
+    const [loading,  setLoading] = useState(true);
+    const [saving, setSaving] = useState(false);
+    const [deleting, setDeleting] = useState(false);
+
+    const [formData, setFormDate] = useState({
+        title: "",
+        description: "",
+        dateOfBirth: "",
+        timeOfBirth: "",
+        placeOfBirth: "",
+    });
 }
