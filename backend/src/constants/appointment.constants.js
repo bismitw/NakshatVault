59c1d225f18850f23ffa8c0a3ab37478a4de19e7
Bismit Wagle <bismitwagle@gmail.com>
2026-04-21 15:14:22 +0530

    feat: add appointment constants

---
+++ backend/src/constants/appointment.constants.js
@@ -0,0 +1,30 @@
+const APPOINTMENT_CONSULTATION_TYPES = [
+    "General",
+    "Marriage",
+    "Career",
+    "Finance",
+    "Health",
+    "Other",
+];
+
+const APPOINTMENT_MODES = ["Online", "Offline"];
+
+const APPOINTMENT_TIME_SLOTS = [
+    "09:00 AM - 09:30 AM",
+    "09:30 AM - 10:00 AM",
+    "10:00 AM - 10:30 AM",
+    "10:30 AM - 11:00 AM",
+    "11:00 AM - 11:30 AM",
+    "11:30 AM - 12:00 PM",
+    "12:00 PM - 12:30 PM",
+    "12:30 PM - 01:00 PM",
+    "01:00 PM - 01:30 PM",
+    "01:30 PM - 02:00 PM",
+];
+
+export {
+    APPOINTMENT_CONSULTATION_TYPES,
+    APPOINTMENT_MODES,
+    APPOINTMENT_TIME_SLOTS,
+};
+
