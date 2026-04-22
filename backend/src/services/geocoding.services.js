82efee273e647eab618d85c1569b42a2b1f89954
Bismit Wagle <bismitwagle@gmail.com>
2026-04-22 10:55:17 +0530

    feat: initialize getLocationDataFromPlace, try to fetch from geoapify with params

--- backend/src/services/geocoding.services.js
+++ backend/src/services/geocoding.services.js
@@ -1,2 +1,19 @@
 import axios from "axios";
 import { ApiError } from "../utils/apiError.utils.js";
+
+
+const getLocationDataFromPlace = async (placeOfBirth) => {
+    try {
+        
+        const response = await axios.get("https://api.geoapify.com/v1/geocode/search", {
+            params: {
+                text: placeOfBirth,
+                format: "json",
+                apiKey: process.env.GEOAPIFY_API_KEY,
+            },
+        });
+
+    } catch (error) {
+        
+    }
+}
