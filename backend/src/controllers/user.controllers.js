77d015c660a200784579b173b1464fa8089240c2
Bismit Wagle <bismitwagle@gmail.com>
2026-04-24 12:37:12 +0530

    feat: initialize getAllUsers for getting all registered user for the product

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,6 +1,6 @@
 import { ApiResponse } from "../utils/apiResponse.utils.js"
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
-import { updateUserProfileService, updateBirthDetailsService } from "../services/user.services.js";
+import { getAllUsersService, updateUserProfileService, updateBirthDetailsService } from "../services/user.services.js";
 
 const getUserProfile = asyncHandler(async (req, res) => {
     return res 
@@ -23,4 +23,12 @@ const updateBirthDetails = asyncHandler(async(req, res) => {
     .json(new ApiResponse(200, updatedUser, "Birth details updated successfully"))
 })
 
-export { getUserProfile, updateUserProfile, updateBirthDetails };
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await getAllUsersService();
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, users, "Users fetched successfully"));
+});
+
+export { getUserProfile, updateUserProfile, updateBirthDetails, getAllUsers };
