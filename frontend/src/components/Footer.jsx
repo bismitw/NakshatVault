73ce1cf4c81202d46ccbc2ce5652c85e89f31b91
Bismit Wagle <bismitwagle@gmail.com>
2026-04-28 19:23:20 +0530

    feat: change bg

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { exp } from "three/src/nodes/math/MathNode.js";
 function Footer() { 
 
 return (
-    <section className="py-10 sm:pt-16 lg:pt-24">
+    <section className="py-10 sm:pt-16 lg:pt-24 bg-gray-950">
     <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
         <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
