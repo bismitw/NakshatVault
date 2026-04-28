6895c0f0d1c9d89b5e52778ef05b662107ae39fd
Bismit Wagle <bismitwagle@gmail.com>
2026-04-28 15:48:25 +0530

    feat: add proper spacing in back button, and justify content center

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -52,16 +52,16 @@ function RegisterPage() {
             <button
                 type="button"
                 onClick={() => navigate(-1)}
-                className="inline-flex items-center gap-2 text-sm text-stone-300 transition hover:text-stone-100"
+                className="inline-flex items-center gap-2 text-sm text-stone-300 transition hover:text-stone-100 cursor-pointer mb-5"
                 aria-label="Go back"
             >
                 <FiArrowLeft size={18} />
                 Back
             </button>
-            <p className="text-xs uppercase tracking-[0.35em] text-amber-300">
+            <p className="text-xs uppercase tracking-[0.35em] text-amber-300 flex justify-center">
             Begin Your Journey
             </p>
-            <h1 className="mt-4 text-3xl font-semibold text-stone-100">Register</h1>
+            <h1 className="mt-4 text-3xl font-semibold text-stone-100 flex justify-center">Register</h1>
 
             <form onSubmit={handleSubmit} className="mt-8 space-y-5">
             <div>
